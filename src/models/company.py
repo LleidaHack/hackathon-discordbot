80e2840968ab50332bcec273daa1df2fd352f14b
Paula Gallucci <personadelmonton@gmail.com>
2020-10-19 12:08:39 +0200

    Añadidos los atributos de clase de los modelos

--- src/models/company.py
+++ src/models/company.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-
+from user import User
 class Company:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        self.users = []
         break
+
+    def add_user(self, user: User):
+        self.users.append(user)
+
+    def remove_user(self, user:User):
+        self.users.remove(user)
--- src/models/team.py
+++ src/models/team.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-
+from user import User 
+from challenge import Challenge
 class Team:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        self.members = []
+        self.challenges = []
         break
+
+    def add_user(self, user: User):
+        self.members.append(user)
+    
+    def kick_user(self, user: User):
+        self.members.remove(user)
+    
+    def join_challenge(self, challenge: Challenge):
+        self.challenges.append(Challenge)
+    
+    def leave_challenge(self, challenge: Challenge):
+        self.challenges.remove(Challenge)
