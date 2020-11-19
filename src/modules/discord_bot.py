32dd1d6028ccf28437787d0d69aca728c57d4a3d
latra <personadelmonton@gmail.com>
2020-11-19 23:44:22 +0100

    Login command

--- src/modules/discord_bot.py
+++ src/modules/discord_bot.py
@@ -61,10 +61,10 @@ class DiscordBot:
         @self.client.event
         async def on_member_join(member):
             await self.login(member)
-            
+
         @self.client.command()
         async def login(ctx):
-            await self.login(ctx.member)
+            await self.login(ctx.author)
 
     def start(self):
         logging.info("Starting bot!")
