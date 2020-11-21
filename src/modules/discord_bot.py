b188614693d273517ce7fad1f98e09825ea88b9f
latra <personadelmonton@gmail.com>
2020-11-21 21:56:13 +0100

    correccion login

--- src/modules/discord_bot.py
+++ src/modules/discord_bot.py
@@ -267,7 +267,7 @@ class DiscordBot:
                 member = guild.get_member(user.id)
                 if guild:
                     if group:
-                        discord_group = DB.getGroup(group.group_name)
+                        discord_group = DB.get_group(group.group_name)
                         if not discord_group:
                             await  self.create_group_on_server(guild, group)
                             discord_group = group
