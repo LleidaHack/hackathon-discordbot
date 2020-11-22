288e483077fe6ad864fd35708562742508eee7b5
latra <personadelmonton@gmail.com>
2020-11-22 14:18:19 +0100

    modificacion textos

--- src/texts/join_texts.py
+++ src/texts/join_texts.py
@@ -9,8 +9,7 @@ def NOT_ALLOWED_TEAM(team_name):
     return f"No veo que te hayan invitado al grupo {team_name}"
 
 
-ERROR_SERVER="¡Uy! Tengo problemas con Lo Servidor..."
-
-
 def MEMBER_REGISTERED_IN(username, team_name):
     return f"¡Genial, {username}! Ya estas en el grupo {team_name}"
+
+ERROR_SERVER="¡Uy! Tengo problemas con Lo Servidor..."
