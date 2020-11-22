1a01bac1de0bb517595e348c4ed314b1438b5e67
latra <personadelmonton@gmail.com>
2020-11-22 13:51:59 +0100

    refractor jokes

--- src/modules/discord_bot.py
+++ src/modules/discord_bot.py
@@ -329,20 +329,9 @@ class DiscordBot:
                 break
 
     async def joke_command(self, ctx):
-        chistes = [
-            "- ¿Por que los de Lepe ponen internet en la ventana?\n- Para tener windows vista.",
-            "- ¿Cuántos técnicos de Microsoft hacen falta para cambiar una bombilla?\n- Ninguno, es un problema de Hardware.",
-            "- ¿Qué es el hardware?\n- El que recibe los golpes cuando falla el software.",
-            "- ¿Cuál es el plato preferido de los informáticos?\n- Las patatas chips.",
-            "- ¿AlguienSabeComoArreglarLaBarraEspaciadora?",
-            "- ¿Cuál es el virus más extendido del mundo?\n- El Sistema Windows.",
-            "- Abuelo, por qué estás delante del ordenador con los ojos cerrados?\n- Esque me ha pedido que cierre las pestañas.",
-            "- Venga, bueno, aceptamos Windows Vista como Sistema Operativo.",
-            "- ¿Cuál es la canción favorita de un caracol?\n- Despacito.",
-            "- ¿Por qué McDonald's no sirve caracoles?\n- Porque no son comida rápida.",
-            "- Va un caracol y derrapa."]
+        import src.texts.jokes_texts as texts
 
-        response = random.choice(chistes)
+        response = random.choice(texts.jokes)
         await ctx.channel.send(response)
 
     async def leave_command(self, ctx):
