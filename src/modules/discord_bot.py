71b54f598962a42e9f99dcb035bea8b1a34a7b6a
Quim Picó Mora <quimpm99@gmail.com>
2020-11-23 22:16:13 +0100

    Added leave and hotfixed some things

--- src/modules/discord_bot.py
+++ src/modules/discord_bot.py
@@ -101,6 +101,7 @@ class DiscordBot:
         async def leave(ctx):
             if not ctx.guild:
                 ctx.guild = self.client.get_guild(int(os.getenv('GUILD')))
+                ctx.author = ctx.guild.get_member(ctx.author.id)
             leave: LeaveCommand = LeaveCommand(ctx, DB)
             await leave.apply()
 
@@ -234,34 +235,3 @@ class DiscordBot:
         logging.info(f"Añadiendo el rol {role.name} al miembro {member.name}")
         await member.add_roles(role)
         await ctx.send(txt.MEMBER_REGISTERED_IN(member.name, role.name))
-
-    @group_required
-    async def leave_command(self, ctx):
-        from src.modules.facades import ContextFacade
-        import src.texts.leave_texts as txt
-        if not ctx.guild:
-            ctx.guild = self.client.get_guild(int(os.getenv('GUILD')))
-            ctx.author = ctx.guild.get_member(ctx.author.id)
-        fac: ContextFacade = ContextFacade(ctx)
-        logging.info(f"leave command por {fac.get_author().name}")
-        user: ModelUser = DB.get_user_from_id(fac.get_author().id)
-        group = DB.get_group(user.group_name)
-        if not group:
-            logging.error(f"No se ha encontrado el grupo {user.group_name} del usuario {user.get_full_name()}.")
-            await ctx.send(txt.SERVER_ERROR)
-            return
-        group.remove_user(user.discord_id)
-        user.group_name = None
-        DB.create_or_update_user(user)
-        DB.create_or_update_group(group)
-        role = discord.utils.get(ctx.guild.roles, name=group.name)
-        member = ctx.guild.get_member(user.discord_id)
-        await member.remove_roles(role)
-        if group.size() == 0:
-            chanels = list(filter(lambda x: role in x.overwrites, ctx.guild.channels))
-            # if the channel exists
-            for chanel in chanels:
-                await chanel.delete()
-            DB.delete_group(group.name)
-            await role.delete()
-        await ctx.send(txt.LEAVE_MSG(member.name, role.name))
