3894777467e514ba587b93570330b5e6dfbc7d1d
Quim Picó Mora <quimpm99@gmail.com>
2020-11-25 18:08:12 +0100

    testing if test is working

--- testbot/commands/test_joke.py
+++ testbot/commands/test_joke.py
@@ -4,5 +4,8 @@ class TestJoke():
     def __init__(self, interface):
         self.interface = interface
 
-    def run_tests(self):
-        pass
+    async def run_tests(self):
+        await self.test_not_working()   
+
+    async def test_not_working(self):
+        await self.interface.assert_reply_equals("eps!joke", "Aixó vull que peti")
