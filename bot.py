111963ab0cca15d2ab55a2b85ab19d3edd62f189
latra <personadelmonton@gmail.com>
2021-06-27 00:57:04 +0200

    Credentials moved to env variable

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ if len(sys.argv) != 1:
     load_dotenv(sys.argv[1])
 else:
     load_dotenv()
+if not os.path.isfile(os.getenv('GOOGLE_APPLICATION_CREDENTIALS')):
+    with open(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'), 'w') as file:
+        file.write(os.getenv('GOOGLE_CREDENTIALS'))
+        
 from src.modules.discord_bot import DiscordBot
 import logging
 
@@ -13,9 +17,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.debug("Reading env configuration")
 
-    if not os.path.isfile(os.getenv('GOOGLE_APPLICATION_CREDENTIALS')):
-        with open(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'), 'w') as file:
-            file.write(os.getenv('GOOGLE_CREDENTIALS'))
+
     bot = DiscordBot()
     bot.start()
 
