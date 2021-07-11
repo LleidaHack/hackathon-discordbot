391aaa128d3e15193fdd94c61d0f9bb2acdfeaff
Paula G <pgallucci@lleida.net>
2021-07-11 13:06:19 +0200

    Revert "Register_texts"
    This reverts commit 4e5c90947ec5184d419d2d83bffc67b5f8e35dd3.

--- src/texts/register_texts.py
+++
@@ -1,5 +0,0 @@
-from src.texts.const import *
-
-COMMAND_HELP_USAGE = f'Estás olvidando algunos campos... utiliza:\n {COMMAND_PREFIX}register EMAIL NAME LASTNAME [GITHUBURL USERNAME]'
-COMMAND_USER_ALREADY_EXISTS= 'El email que intentas registrar ya existe'
-COMMAND_USER_CREATED='El usuario se ha creado correctamente. Ya puede loguearse con el comando de login'
